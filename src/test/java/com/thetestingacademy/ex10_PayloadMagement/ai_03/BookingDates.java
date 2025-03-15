223bd9a6d97c0049c04235710c0002c311652856
Promode <pramoddutta_96@yahoo.com>
2025-03-15 11:21:08 +0530

    15th March - P2 - p1

---
+++ src/test/java/com/thetestingacademy/ex10_PayloadMagement/ai_03/BookingDates.java
@@ -0,0 +1,23 @@
+package com.thetestingacademy.ex10_PayloadMagement.ai_03;
+
+public class BookingDates {
+    private String checkin;
+    private String checkout;
+
+    // Getters and Setters
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+}
